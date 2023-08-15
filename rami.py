06f360bd6ee094b04d5759879bf9e198286ced8d
joseangelayalaz <ayalazjangel@gmail.com>
2023-08-15 10:26:25 -0600

    Made the binary search problem algorithm

---
+++ rami.py
@@ -0,0 +1,9 @@
+nombre1 = int(input('cual es tu edad Angel: '))
+nombre2 = int(input('cual es tu edad Pedro: '))
+if nombre1 > nombre2:
+    print('Angel es mayor que Pedro')
+elif nombre1 < nombre2:
+    print('Angel es menor que Pedro')
+else:
+    print('Angel y Pedro tienen la misma edad')
+
