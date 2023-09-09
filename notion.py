675f3c211537e8ff0389e600d756849a0f1fd647
joseangelayalaz <ayalazjangel@gmail.com>
2023-09-08 20:38:08 -0600

    prueba de caja de errores

---
+++ notion.py
@@ -0,0 +1,5 @@
+my_list = list(range(100))
+double = [i * 2 for i in my_list]
+pares = [i for i in my_list if i % 2 == 0]
+print(pares)
+
--- range.py
+++ range.py
@@ -3,7 +3,12 @@ n = int(input('Choose: '))
 for i in range(0,n):
     print(i**2)
 '''
+'''
 n = int(input('num: '))
 n += 1
 for i in range(1,n):
     print(i, end='')
+'''
+impar = range(1, 100, 2)
+for i in impar:
+    print(i)
